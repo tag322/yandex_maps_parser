9b432dac90fa9f8d605fb2b848da01af388b210e
tag <taghr322@gmail.com>
2026-06-14 01:17:45 +0500

    seeder w/o fake bundle

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        User::updateOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
